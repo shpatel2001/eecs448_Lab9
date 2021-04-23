84947c36bb7ef786cf14c8df861bb2ee80852c34
shpatel2001 <60803469+shpatel2001@users.noreply.github.com>
2021-04-22 22:11:12 -0500

    Update exercise1.php

--- exercise1/exercise1.php
+++ exercise1/exercise1.php
@@ -1,12 +1,13 @@
 <?php
-
+    $heading = "Multiplication Table: 100x100";
+    echo "<h1>" . $heading . "</h1>";
     echo "<table>";
         for($i = 1; $i < 101; $i++)
         {
-            echo "<tr>";
+            echo "<td></td>";
                 for($j = 1; $j < 101; $j++)
                 {
-                    $x = $i*$j
+                    $x = $i*$j;
                     echo "<td>$x</td>";
                 }
             echo "</tr>";
