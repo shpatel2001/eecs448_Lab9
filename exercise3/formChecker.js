c46c56490290ef466646781d25f3750f53c89499
shpatel2001 <60803469+shpatel2001@users.noreply.github.com>
2021-04-25 20:53:19 -0500

    Add files via upload

---
+++ exercise3/formChecker.js
@@ -0,0 +1,62 @@
+function check()
+{
+  var Item1 = document.getElementById("item1").value;
+  var Item2 = document.getElementById("item2").value;
+  var Item3 = document.getElementById("item3").value;
+  var Ship = document.getElementsByName("Shipping").value;
+  var User = document.getElementById("user").value;
+  var Pass = document.getElementById("pass").value;
+
+  var Item_Check = false;
+  var User_Check = false;
+  var Pass_Check = false;
+
+
+  if(Item1 == "" || Item2 == "" || Item2 == "" || Item1 < 0 || Item2 < 0 || Item3 < 0)
+  {
+    alert("Please selected a quantity for all items");
+    Item_Check = false;
+  }
+  else
+  {
+    Item_Check = true;
+  }
+
+  
+  if(validate(User))
+  {
+    User_Check = true;
+  }
+  else
+  {
+    alert("Invalid email")
+    User_Check = false;
+  }
+
+  
+  if(Pass == "")
+  {
+    alert("Please input your password");
+    Pass_Check = false;
+  }
+  else
+  {
+    Pass_Check = true;
+  }
+
+  
+  if(Item_Check && User_Check && Pass_Check)
+  {
+    return(true);
+  }
+  else
+  {
+    return(false);
+  }
+}
+
+
+function validate(Username)
+{
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Username)
+}
